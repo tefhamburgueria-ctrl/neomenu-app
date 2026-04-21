137b1e4876cfb4d7814f56a8c4d4c399f78a931b
tefhamburgueria-ctrl <tef.hamburgueria@gmail.com>
2026-04-21 19:44:43 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Service Worker para NeoMenu PWA
+const CACHE_NAME = 'neomenu-v1';
+const urlsToCache = [
+  '/neomenu-app/',
+  '/neomenu-app/noemenu-cloud.html',
+  '/neomenu-app/cardapio-online.html',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Busca em cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Atualização do cache
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
